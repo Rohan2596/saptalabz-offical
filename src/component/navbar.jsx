310f2aa9b922ceed3148053c185543578b49f45f
Rohan2596 <rohankadam662@gmail.com>
2020-09-18 16:36:49 +0530

    ADDED:title and menu to navbar.

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,19 +1,17 @@
 import  React,{Component}  from "react";
-import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
-import { FaBars,FaTimes } from "react-icons/fa";
+import './navbar.css'
+import MenuIcon from '@material-ui/icons/Menu';
 
 class Navbar extends Component{
     render(){
         return(
             <>
             <div className="navbar">
-                <div className="navbar-container container">
-                    <Link to="/" className="navbar-logo">
-                        <MdFingerprint  className='navbar-icon' />
-                        LAVISH
-                    </Link>
+                <div className="title">
+                    Saptalabz
+
                 </div>
+                <div className="menuIcon"><MenuIcon/></div>
             </div>
 
             </>
