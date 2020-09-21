fc9be46a137a79167288a2ca143fd81bad436ee9
Rohan2596 <rohankadam662@gmail.com>
2020-09-21 13:12:03 +0530

    Updates

---
+++ precache-manifest.880a74cb5f619daa6b6be690b2e92231.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458dbc775ae9a5a799d1d6f57dc9d191",
+    "url": "/Rohan2596.github.io/saptalabz-offical/index.html"
+  },
+  {
+    "revision": "83262bb48de433f7d009",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/css/main.aae9154a.chunk.css"
+  },
+  {
+    "revision": "edd0080e1f5591f217d4",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/js/2.92c4c0e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/js/2.92c4c0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83262bb48de433f7d009",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/js/main.550d6ca3.chunk.js"
+  },
+  {
+    "revision": "06364d5f51d7df95faa9",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/js/runtime-main.506d5147.js"
+  },
+  {
+    "revision": "27fe5be6c33be01d1c0567b0747b553e",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/media/background.27fe5be6.png"
+  },
+  {
+    "revision": "f4638af9bdf9909a4464813ca1034f81",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/media/marlon.f4638af9.png"
+  },
+  {
+    "revision": "401b8b0a6b9dde995cf11b1c8202d92d",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/media/team.401b8b0a.png"
+  },
+  {
+    "revision": "9b4319c15888cf545cc28aed29968140",
+    "url": "/Rohan2596.github.io/saptalabz-offical/static/media/user_flow.9b4319c1.gif"
+  }
+]);
