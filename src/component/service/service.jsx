c30056d94c67cc79bc1584bfd6a569b1f474fcdd
Rohan2596 <rohankadam662@gmail.com>
2020-09-21 12:34:38 +0530

    WIP:Service page of Websites.

---
+++ src/component/service/service.jsx
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+import './service.css'
+
+
+class Service extends Component{
+
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+
+}
+export default Service;
