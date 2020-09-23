8c2a16bc5b4797ec305278ff5016f23ea293ba92
Rohan2596 <rohankadam662@gmail.com>
2020-09-23 18:58:13 +0530

    REFRACTED:Navbar section redirection of into navbar application.

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,56 +1,75 @@
-import  React,{Component}  from "react";
+import React, { Component } from "react";
 import './navbar.css'
 import MenuIcon from '@material-ui/icons/Menu';
-
-class Navbar extends Component{
-    constructor(props){
+import Home from '../component/home/home';
+import AboutUs from '../component/about-us/aboutUs';
+import Service from '../component/service/service';
+import ContactUs from '../component/contactUs/contactUs';
+class Navbar extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            isToggleOn:false
+        this.state = {
+            isToggleOn: false
         }
     }
-    menuclick(){
+    menuclick() {
         this.setState(state => ({
-            isToggleOn:true
+            isToggleOn: true
         }));
-        console.log("Menu click"+this.state.isToggleOn);
+        console.log("Menu click" + this.state.isToggleOn);
         console.log("sdfnsdnfns");
-        
-        
+
+
     }
-   
-    render(){
-        return(
+
+    render() {
+        return (
             <>
-            <div className="navbar">
-                <div className="title">
-                    Saptalabz
+                <div className="navbar">
+                    <div className="title">
+                        Saptalabz
 
                 </div>
-                <div className="menuIcon" ><MenuIcon onClick={() => this.menuclick()}
-                style={{
-                    alignItems:'center'
-                    ,display:"flex"
-                }}/></div>
+                    <div className="menuIcon" ><MenuIcon onClick={() => this.menuclick()}
+                        style={{
+                            alignItems: 'center'
+                            , display: "flex"
+                        }} /></div>
 
-                <div className="menulist">
-                    <span  href="home"  className="menuItem">
-                        Home
-                    </span>
-                    <span href="about" className="menuItem">
-                        About Us
-                    </span>
-                    <span href="" className="menuItem">
-                        Our Services
-                    </span>
-                    <span className="menuItem">
-                        Contact Us
-                    </span>
+                    <div className="menulist">
+                        <a href="#home" className="menuItem">
+                            Home
+                    </a>
+                        <a href="#about" className="menuItem">
+                            About Us
+                    </a>
+                        <a href="#service" className="menuItem">
+                            Our Services
+                    </a>
+                        <a  href="#contact" className="menuItem">
+                            Contact Us
+                    </a>
+                    </div>
                 </div>
-            </div>
-          
+                <section id="home">
+                    <Home />
+
+                </section>
+                <section id="about">
+                    <AboutUs />
+
+                </section>
+                <section id="service">
+                    <Service />
+
+                </section>
+                <section id="contact">
+                    <ContactUs />
+
+                </section>
+
             </>
         )
     }
 }
- export default Navbar
+export default Navbar
